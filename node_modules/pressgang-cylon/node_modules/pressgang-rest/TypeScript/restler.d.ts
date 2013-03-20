6094f61c7327871b8dc2a103c7acc9f55920deb7
Joshua Wulf <jwulf@redhat.com>
2013-03-19 22:21:02 -0400

    added something

---
+++ node_modules/pressgang-cylon/node_modules/pressgang-rest/TypeScript/restler.d.ts
@@ -0,0 +1,5 @@
+///<reference path='node.d.ts' />
+
+declare module "restler" {
+    export function get(url: string);
+}
--- public/scripts/add-book.js
+++ public/scripts/add-book.js
@@ -1,6 +1,7 @@
 
 
 function addBook () {
+     $('#result').html('Checking out book on server');
     $.get('/rest/1/addbook', {url: $('input[name="serverURL"]').val(), id: $('input[name="specID"]').val() }, 
     function(response){
         $('#result').html(response.msg);
