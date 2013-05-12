83ef39fabfb72659e795ecc2a3c61b91398c2d60
Joshua Wulf <jwulf@redhat.com>
2013-05-12 22:51:37 +1000

    Fixes #9. Endpoint for latest package in brew. Integrated with client publican brand

--- routes/restapi.js
+++ routes/restapi.js
@@ -8,6 +8,8 @@ var pressgang = require('pressgang-rest'),
     topics = require('./../lib/topicdriver'),
     assembler = require('./../lib/assembler'),
     wrench = require('wrench'),
+    xmlrpc = require('xmlrpc'),
+    settings = require('./../lib/settings'),
     LOG_HEADER = ephemeral.LOG_HEADER,
     LOG_CONSOLE = ephemeral.LOG_CONSOLE;
 
@@ -29,35 +31,67 @@ console.log("Loaded stylesheet in rest router");
 function restroute (req, res){
     var op = req.params.operation;
     console.log('Rest router called: ' + op);
-    if (op === 'dtdvalidate') {dtdvalidate.dtdvalidate (req, res);}
-    else
-    if (op === 'buildbook') {buildbook(req, res);}
-    else
-    if (op === 'buildstatus') {buildstatus(req, res);}
-    else 
-    if (op ==='addbook') {addbook(req, res);}
-    else
-    if (op ==='xmlpreview') {xmlpreview.xmlPreview(req, res);}
-    else 
-    if (op =='remove') {removebook(req, res);}
-    else
-    if (op == 'patch') {livePatch.patchRESTendpoint(req, res);}
-    else
-    if (op == 'rebuildAll') {rebuildAll(req, res);}
-    else
-    if (op == 'build') {build(req, res);}
-    else 
-    if (op == 'publish') {publish(req, res);}
-    else
-    if (op == 'stopPublish') {stopPublish(req, res);}
-    else
-    if (op == 'getBookmd') {getBookmd(req, res);}
-    else
-    if (op == 'topicupdate') {topics.topicupdate(req, res)}
-    else 
-    if (op =='gettopic') {topics.gettopicRESTEndpoint(req,res);}
-    else
-    if (op === 'getbooks') {getBooks(req, res);};
+    if (op === 'dtdvalidate') { dtdvalidate.dtdvalidate (req, res);
+        } else
+    if (op === 'buildbook') { buildbook(req, res); }
+        else
+    if (op === 'buildstatus') { buildstatus(req, res); }
+        else
+    if (op === 'addbook') { addbook(req, res); }
+        else
+    if (op === 'xmlpreview') { xmlpreview.xmlPreview(req, res); }
+        else
+    if (op === 'remove') { removebook(req, res); }
+        else
+    if (op === 'patch') { livePatch.patchRESTendpoint(req, res); }
+        else
+    if (op === 'rebuildAll') { rebuildAll(req, res); }
+        else
+    if (op === 'build') { build(req, res); }
+        else
+    if (op === 'publish') { publish(req, res); }
+        else
+    if (op === 'stopPublish') { stopPublish(req, res); }
+        else
+    if (op === 'getBookmd') { getBookmd(req, res); }
+        else
+    if (op === 'topicupdate') { topics.topicupdate(req, res); }
+        else
+    if (op === 'gettopic') { topics.gettopicRESTEndpoint(req,res); }
+        else
+    if (op === 'getbooks') { getBooks(req, res); }
+        else
+    if (op === 'getlatestpackagenumber') { getLatestPackageNumber(req,res); }
+}
+
+function getLatestPackageNumber(req, res) {
+    var client, my, packageName;
+
+    client= xmlrpc.createClient({host:  settings.brewhost , port: 80, path: '/brewhub'});
+    req.query.url && req.query.id && (my = Library.Books[req.query.url][req.query.id]);
+    my && (packageName = my.product.split(' ').join('_') + '-' +
+    my.title.split(' ').join('_') + '-' +
+    my.version + '-web-en-US');
+
+    if (!packageName) { return res({code: 1, msg: "Can't find that book."}); }
+
+    console.log('Locating package number for ' + packageName);
+    client.methodCall('getLatestBuilds', [settings.brewDocsTag, {'__starstar': 1, 'package': packageName}], function (error, result){
+        if (error) {
+            return res.send({code: 1, msg: error}); // and bail
+        }
+        if (result.length === 0) {
+            return res.send({code:2, msg: 'Does that package exist?'});
+        }
+        var pubsnum = result[0].release; // looks something like '79.el6eng'
+        pubsnum = pubsnum.substr(0, pubsnum.indexOf('.')); // just get the pubsnum
+        pubsnum = parseInt(pubsnum, 10); // turn it into a number so we can increment it
+        pubsnum ++;
+
+        return res.send({code:0, pubsnum: my.version + '-' + pubsnum});
+
+        //$('revision revnumber').first().text(my.version + '-' + pubsnum);
+    });
 }
 
 function getBooks(req,res) {
